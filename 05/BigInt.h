cc8f1a7a352968c5df89fe25fe1b9b0ff1476349
zakhvataev <zahvataevm@inbox.ru>
2020-04-12 17:18:44 +0300

    _

---
+++ 05/BigInt.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+
+
+class BigInt {
+    char* num;
+    int size;
+    bool sign;
+public:
+    BigInt();
+    BigInt(int);
+    BigInt(const BigInt&);
+
+    BigInt plus(const BigInt& right) const;
+    BigInt minus(const BigInt& right) const;
+    bool abs_(const BigInt& right) const;
+
+    BigInt& operator=(const BigInt&);
+    BigInt operator+(const BigInt&);
+    BigInt operator-() const;
+    BigInt operator-(const BigInt&);
+
+    friend std::ostream& operator<<(std::ostream& , const BigInt&);
+    friend bool operator==(const BigInt&, const BigInt&);
+    friend bool operator!=(const BigInt&, const BigInt&);
+    friend bool operator>(const BigInt&, const BigInt&);
+    friend bool operator<(const BigInt&, const BigInt&);
+    friend bool operator>=(const BigInt&, const BigInt&);
+    friend bool operator<=(const BigInt&, const BigInt&);
+
+    ~BigInt();
+};
